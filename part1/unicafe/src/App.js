9211bb6ff951802f9b709d83f42cb11103573c34
Jack Zhang <jack333zhang@gmail.com>
2022-11-20 21:17:18 +0800

    Part1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,15 +11,14 @@ const Display = (props) => (
 )
 
 
-const Statistic = (props) => (
-    <div>{props.text} {props.func()}</div>
-)
+const Statistic = (props) => {
+    const good = props.good
+    const neutral = props.neutral
+    const bad = props.bad
 
-const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    if (good === 0 && neutral === 0 && bad === 0) {
+        return <div>No feedback given</div>
+    }
 
     const all = () => good + neutral + bad
     const average = () => {
@@ -39,17 +38,30 @@ const App = () => {
 
     return (
         <div>
+            <Display text='good' value={good} />
+            <Display text='neutral' value={neutral} />
+            <Display text='bad' value={bad} />
+            <Display text='all' value={all()} />
+            <Display text='average' value={average()} />
+            <Display text='positive' value={positive()} />
+        </div>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
             <h2>give feedback</h2>
             <Button handleClick={() => setGood(good + 1)} text='good' />
             <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
             <Button handleClick={() => setBad(bad + 1)} text='bad' />
             <h2>statistics</h2>
-            <Display text='good' value={good} />
-            <Display text='neutral' value={neutral} />
-            <Display text='bad' value={bad} />
-            <Statistic text='all' func={all} />
-            <Statistic text='average' func={average} />
-            <Statistic text='positive' func={positive} />
+            <Statistic good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
