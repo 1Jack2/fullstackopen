087ed10c45f7785119a567291c51084dd16b9cb2
Jack Zhang <jack333zhang@gmail.com>
2022-11-21 22:57:14 +0800

    Part2.4: Course information step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 const Head = (props) => (
-    <h1>{props.title}</h1>
+    <h2>{props.title}</h2>
 )
 
 const Part = (props) => (
@@ -12,11 +12,16 @@ const Content = ({parts}) => (
     )
 )
 
-const Course = ({course}) => (
+const Course = ({courses}) => (
     <div>
-        <Head title={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
+        <h1> Web development curriculum </h1>
+        {courses.map(course =>
+            <div key={course.id}>
+                <Head title={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts} />
+            </div>
+        )}
     </div>
 )
 
@@ -32,29 +37,52 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    return <Course course={course} />
+    return <Course courses={courses} />
 }
 
 export default App
