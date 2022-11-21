a5c356ab1907dbf8d3ebab8bb3f62aa8ab92cacb
Jack Zhang <jack333zhang@gmail.com>
2022-11-21 23:03:06 +0800

    Part2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,40 +1,4 @@
-const Head = (props) => (
-    <h2>{props.title}</h2>
-)
-
-const Part = (props) => (
-    <p>{props.part} {props.exercises}</p>
-)
-
-const Content = ({parts}) => (
-    parts.map(part =>
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-    )
-)
-
-const Course = ({courses}) => (
-    <div>
-        <h1> Web development curriculum </h1>
-        {courses.map(course =>
-            <div key={course.id}>
-                <Head title={course.name} />
-                <Content parts={course.parts} />
-                <Total parts={course.parts} />
-            </div>
-        )}
-    </div>
-)
-
-const Total = ({parts}) => {
-    return (
-        <p><b>
-            Total of exercises {parts.reduce(
-                (s, p) => s + p.exercises,
-                0
-            )}
-        </b></p>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
