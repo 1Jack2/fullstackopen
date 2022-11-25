42e02aa3b6c30345b8c6f1d56e8613d7c35207b4
Jack Zhang <jack333zhang@gmail.com>
2022-11-25 22:25:26 +0800

    Part2.19: Phonebook step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import service from './services/phonebook'
+import Notification from './notification'
 
 const App = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterKey, setFilterKey] = useState('')
+    const [notification, setNotification] = useState(null)
     const filteredPersons = persons.filter(
         (person) => person.name.toLowerCase().includes(filterKey.toLowerCase()));
 
@@ -26,13 +28,19 @@ const App = () => {
         if (existedPerson) {
             const ok = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
             if (ok) {
-                const updatedPerson = { ...existedPerson, number: newNumber }
+                const updatedPerson = {...existedPerson, number: newNumber}
                 console.log(updatedPerson)
                 service.update(existedPerson.id, updatedPerson).then((data) => {
+                    setNotification(
+                        `Updated ${data.name}`
+                    )
+                    setTimeout(() => {
+                        setNotification(null)
+                    }, 3000)
                     setPersons(persons.map((v) => {
                         return v === existedPerson
                             ? updatedPerson
-                            : data
+                            : v
                     }))
                 })
             }
@@ -45,6 +53,12 @@ const App = () => {
             number: newNumber,
         }
         service.create(newPerson).then(data => {
+            setNotification(
+                `Added ${data.name}`
+            )
+            setTimeout(() => {
+                setNotification(null)
+            }, 5000)
             setPersons(persons.concat(data))
         })
     }
@@ -72,6 +86,7 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={notification} />
             <Filter filterKey={filterKey} handleFilterChange={handleFilterChange} />
             <h3>add a new</h3>
             <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
